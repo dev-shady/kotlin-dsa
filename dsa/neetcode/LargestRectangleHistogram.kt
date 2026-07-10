9c27784b743b41b980db88a9ce89ac042f88e7a6
Amit Kumar <itsdevshady@gmail.com>
2026-07-10 22:16:02 +0530

    added single pass optimized solution

--- dsa/neetcode/LargestRectangleHistogram.kt
+++ dsa/neetcode/LargestRectangleHistogram.kt
@@ -72,10 +72,64 @@ class LargestRectangleHistogram {
 
         return maxArea
     }
+
+    fun largestRectangleAreaOptimal(heights: IntArray): Int {
+        val stack = Stack<Int>()
+        val n = heights.size
+        val left = IntArray(n)
+        var right = IntArray(n)
+        var curLeft = -1
+        var  curRight = -1
+        for (i in 0 until n) {
+            if (stack.isEmpty()) {
+                left[i] = i
+                stack.push(i)
+                continue
+            }
+
+            var top = stack.peek()
+            val cur = heights[i]
+            curLeft = i
+
+            while (heights[top] >= cur) {
+                right[top] = i-1
+                stack.pop()
+                if (stack.isEmpty()) {
+                    curLeft = 0
+                    break
+                }
+                top = stack.peek()
+                curLeft = top+1
+            }
+
+            left[i] = curLeft
+            stack.push(i)
+        }
+
+        while (stack.isNotEmpty()) {
+            var cur = stack.pop()
+            right[cur] = n-1
+        }
+
+        for ( i in n-1 downTo 1 ) {
+            if (heights[i] == heights[i-1]) {
+                right[i-1] = right[i]
+            }
+        }
+
+        var maxArea = 0
+        for (i in 0 until n) {
+            maxArea = Math.max(maxArea, (right[i] -left[i] +1)* heights[i])
+        }
+
+        return maxArea
+    }
 }
 
 fun main() {
-    println(LargestRectangleHistogram().largestRectangleArea(intArrayOf(7,1,7,2,2,4)))
-    println(LargestRectangleHistogram().largestRectangleArea(intArrayOf(1,3,7)))
+    println(LargestRectangleHistogram().largestRectangleAreaOptimal(intArrayOf(7,1,7,2,2,4)))
+    println(LargestRectangleHistogram().largestRectangleAreaOptimal(intArrayOf(1,3,7)))
+    println(LargestRectangleHistogram().largestRectangleAreaOptimal(intArrayOf(3,7,4,4,4,4,4,2,4)))
+
 
 }
